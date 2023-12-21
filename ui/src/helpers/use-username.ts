9b003279b68a570b8ce72c6ebaf194bc2deb701b
fibre-ether <fire.breather@rediff.com>
2023-12-21 21:03:50 +0530

    fix issue with username not fetching from localstorage

--- ui/src/helpers/use-username.ts
+++ ui/src/helpers/use-username.ts
@@ -2,7 +2,7 @@ function UseUsername() {
   let userName = "anon";
   // const [userName, setUserName] = useState<string>("anon");
   // useEffect(() => {
-  let localStorageUserName = "anon-12345"; //localStorage.getItem("username")
+  let localStorageUserName = localStorage.getItem("username")
 
   if (!localStorageUserName) {
     localStorageUserName = "anon-" + Math.floor(Math.random() * 100000 + 9999);
